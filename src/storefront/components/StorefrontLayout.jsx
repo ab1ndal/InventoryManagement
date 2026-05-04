66f2a321148103d2dd9e31255e40cb95e146fa34
abindal6@gatech.edu <abindal6@gatech.edu>
2026-05-03 18:02:48 -0700

    feat(storefront): wrap StorefrontLayout in CartProvider, render CartDrawer

--- src/storefront/components/StorefrontLayout.jsx
+++ src/storefront/components/StorefrontLayout.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import StorefrontHeader from "./StorefrontHeader";
 import StorefrontFooter from "./StorefrontFooter";
+import { CartProvider } from "../context/CartContext";
+import CartDrawer from "./cart/CartDrawer";
 
 export default function StorefrontLayout() {
   return (
-    <div className="min-h-dvh bg-storefront-cream font-montserrat text-storefront-charcoal">
-      <StorefrontHeader />
-      <main>
-        <Outlet />
-      </main>
-      <StorefrontFooter />
-    </div>
+    <CartProvider>
+      <div className="min-h-dvh bg-storefront-cream font-montserrat text-storefront-charcoal">
+        <StorefrontHeader />
+        <main>
+          <Outlet />
+        </main>
+        <StorefrontFooter />
+        <CartDrawer />
+      </div>
+    </CartProvider>
   );
 }
