9f1037b75262607b75e7686d568aa94aee6f2093
abindal6@gatech.edu <abindal6@gatech.edu>
2026-05-30 18:48:35 -0700

    Updated date in exchange

--- src/utility/dateFormat.js
+++ src/utility/dateFormat.js
@@ -2,6 +2,7 @@ import { format } from "date-fns";
 
 export const formatDate = (dateStr) => {
     if (!dateStr) return "-";
+    if (dateStr instanceof Date) return format(dateStr, "dd/MM/yyyy");
     if (/^\d{2}\/\d{2}\/\d{4}$/.test(dateStr)) return dateStr;
     const d = dateStr.includes("T") ? new Date(dateStr) : new Date(dateStr + "T00:00:00+05:30");
     return format(d, "dd/MM/yyyy");
