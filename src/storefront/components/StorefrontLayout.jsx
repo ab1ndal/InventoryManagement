6a2061ca6f3d489c118227bbe4e361a439c1d636
abindal6@gatech.edu <abindal6@gatech.edu>
2026-07-10 18:22:38 -0700

    feat(auth): storefront auth context (email magic link)

--- src/storefront/components/StorefrontLayout.jsx
+++ src/storefront/components/StorefrontLayout.jsx
@@ -3,19 +3,22 @@ import { Outlet } from "react-router-dom";
 import StorefrontHeader from "./StorefrontHeader";
 import StorefrontFooter from "./StorefrontFooter";
 import { CartProvider } from "../context/CartContext";
+import { StorefrontAuthProvider } from "../context/StorefrontAuthContext";
 import CartDrawer from "./cart/CartDrawer";
 
 export default function StorefrontLayout() {
   return (
-    <CartProvider>
-      <div className="min-h-dvh bg-storefront-cream font-sans text-storefront-charcoal">
-        <StorefrontHeader />
-        <main>
-          <Outlet />
-        </main>
-        <StorefrontFooter />
-        <CartDrawer />
-      </div>
-    </CartProvider>
+    <StorefrontAuthProvider>
+      <CartProvider>
+        <div className="min-h-dvh bg-storefront-cream font-sans text-storefront-charcoal">
+          <StorefrontHeader />
+          <main>
+            <Outlet />
+          </main>
+          <StorefrontFooter />
+          <CartDrawer />
+        </div>
+      </CartProvider>
+    </StorefrontAuthProvider>
   );
 }
